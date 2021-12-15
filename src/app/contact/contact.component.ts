8e4529cb91bf858ac0e88860c2809abe9b853aac
mubashir-dev <mubashirmobi633@gmail.com>
2021-12-15 21:47:02 +0500

    form features has completed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {Contact} from '../contact';
 
 @Component({
   selector: 'app-contact',
@@ -8,12 +9,21 @@ import {FormControl} from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
   title: string | undefined;
+  contact: Contact = {
+    name: 'dd',
+    email: '',
+    message: ''
+  };
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.title = 'Contact Us'
+  }
+
+  submitContact() {
+    console.log('Contact : ', this.contact);
   }
 
 }
